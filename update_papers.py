5f418db7c07eca81a5c7e4567a4a8b16ab9e0fa5
Lina Tran <lina.mntran@gmail.com>
2018-04-03 22:02:24 -0400

    autopopulate publications

---
+++ update_papers.py
@@ -0,0 +1,121 @@
+from Bio import Entrez
+
+
+def parse_author(author_info):
+    last = author_info['LastName']
+    forename = author_info['ForeName']
+    name = ", ".join([last, forename])
+    return name
+
+
+def parse_journal(journal_info, pagination):
+    abbrev = journal_info['ISOAbbreviation']
+    if journal_info.has_key('JournalIssue'):
+        issue = journal_info['JournalIssue']
+        if pagination and issue.has_key('Issue'):
+            issue = ', '.join([abbrev, issue['Volume'], issue['Issue'], pagination['MedlinePgn']])
+        else:
+            issue = ', '.join([abbrev, issue['Volume']])
+    else:
+        issue = abbrev
+    return issue
+
+class Paper(object):
+    def __init__(self, pubmed_entry):
+        article_info = pubmed_entry['MedlineCitation']['Article']
+        #author info
+        authors = article_info['AuthorList']
+        if len(authors) < 3:
+            self.first_author = ', '.join([parse_author(author) for author in authors]) + '.'
+        else:
+            self.first_author = parse_author(authors[0]) + ' et al.'
+        #article info
+        if len(article_info['ArticleDate']):
+            self.year = article_info['ArticleDate'][0]['Year']
+        else:
+            self.year = article_info['Journal']['JournalIssue']['PubDate']['Year']
+        self.title = article_info['ArticleTitle']
+        pagination = False
+        if article_info.has_key("Pagination"):
+            pagination = article_info['Pagination']
+        self.journal_info = parse_journal(article_info['Journal'], pagination)
+        if len(article_info["ELocationID"]):
+            self.link = 'https://doi.org/' + article_info['ELocationID'][0]
+        else:
+            self.link = ''
+        self.yml = """- author: {}\n  title: "{} {}."\n  alt_link: "{}"\n  year: {}\n\n".format(self.first_author, self.title, self.journal_info, self.link, self.year)"""
+
+
+def search_author(query, email, max_search=20):
+    Entrez.email = email
+    handle = Entrez.esearch(db='pubmed',
+                            retmax=max_search,
+                            retmode='XML',
+                            term=query)
+    results = Entrez.read(handle)
+    return results['IdList']
+
+
+
+def fetch_details(id_list, email):
+    ids = ','.join(id_list)
+    Entrez.email = email
+    handle = Entrez.efetch(db='pubmed',
+                           retmode='xml',
+                           id=ids)
+    results = Entrez.read(handle)
+    return results['PubmedArticle']
+
+def fetch_new_papers(ids, email):
+    '''
+    Fetch only new papers, and update the recent_pubmed_ids.txt list.
+    If no new papers found, stop.
+    '''
+    a = open('_data/recent_pubmed_ids.txt', 'r')
+    previous_ids = a.read().split(',')
+    a.close()
+    ids = [i for i in ids if i not in previous_ids]
+
+    if len(ids) > 0:
+        #only fetch if there are new ids
+        results = fetch_details(ids, email)
+        papers = [Paper(i).yml for i in results]
+
+
+        #update recent_pubmed_ids
+        a = open('_data/recent_pubmed_ids.txt', 'w')
+        a.write(','.join(ids))
+        a.close()
+
+        return papers
+    else:
+        print("No new papers found.")
+
+
+def add_new_papers(new_papers):
+    if new_papers:
+        append_copy = open("_data/papers.yml", "r")
+        original_text = append_copy.read()
+        append_copy.close()
+
+        new_papers = ''.join(new_papers)
+
+        append_copy = open("_data/papers.yml", "w")
+        append_copy.write(new_papers)
+        append_copy.write(original_text)
+        append_copy.close()
+
+
+def main():
+    user_email = 'lina.mntran@gmail.com'
+    author = 'Frankland PW[Author]'
+
+    ids = search_author(author, user_email)
+    papers = fetch_new_papers(ids, user_email)
+    if papers is not None:
+        add_new_papers(papers)
+        print("Added {} new papers.".format(len(papers)))
+
+
+if __name__ == '__main__':
+    main()
